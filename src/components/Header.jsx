2dc49e438c7ea6907103ba536731f6f45566c815
5450L <92631220+5450L@users.noreply.github.com>
2022-12-07 13:31:44 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <img src="https://www.pngmart.com/files/13/Akatsuki-Logo-PNG-Pic.png"/>
+        </header>
+    )
+}
+
+export default Header;

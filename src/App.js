de8083f087a1ff253d109cfdaa7c2873a8ebeca2
5450L <92631220+5450L@users.noreply.github.com>
2022-12-21 11:30:49 +0200

    +router params

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import Header from "./components/Header.component/Header";
 import Nav from "./components/Nav.component/Nav";
 // import Content from "./components/Content.component/Content";
 import Dialogs from "./components/Dialogs.component/Dialogs";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import UsersContainer from "./components/Users.component/UsersContainer";
 import ContentContainer from "./components/Content.component/ContentContainer";
 
 const App = function (props) {
-  return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Nav />
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route path="/dialogs" element={<Dialogs store={props.store} />} />
-            <Route path="/profile/*" element={<ContentContainer />} />
-            <Route path="/users" element={<UsersContainer />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Nav/>
+                <div className="app-wrapper-content">
+                    <Routes>
+                        <Route path="/dialogs" element={<Dialogs store={props.store}/>}/>
+                        <Route path="/profile/:userId?" element={<ContentContainer/>}/>
+                        <Route path="/users" element={<UsersContainer/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 };
 
 export default App;
--- src/components/Content.component/ContentContainer.jsx
+++ src/components/Content.component/ContentContainer.jsx
@@ -2,26 +2,52 @@ import React from "react";
 import contentStyles from "./Content.module.css";
 import Content from "./Content";
 import axios from "axios";
-import { connect } from "react-redux";
-import { setUserProfile } from "../../redux/reducers/profile-reducer";
+import {connect} from "react-redux";
+import {setUserProfile} from "../../redux/reducers/profile-reducer";
+import {useLocation, useNavigate, useParams} from "react-router";
+
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{location, navigate, params}}
+            />
+        );
+    };
+
+    return ComponentWithRouterProp;
+}
 
 class ContentContainer extends React.Component {
-  componentDidMount() {
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-      .then((response) => {
-        console.log(this.props);
-        this.props.setUserProfile(response.data);
-      });
-  }
 
-  render() {
-    return (
-      <div className={contentStyles.content}>
-        <Content {...this.props} profile={this.props.profile}></Content>
-      </div>
-    );
-  }
-}
-let MapStateToProps = (state) => ({ profile: state.profilePage.profile });
-export default connect(MapStateToProps, { setUserProfile })(ContentContainer);
+
+    componentDidMount() {
+
+        let userId = this.props.router.params.userId;
+        console.log(this.props.router.params.userId);
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
+            .then((response) => {
+                console.log(this.props);
+                this.props.setUserProfile(response.data);
+            });
+    };
+
+    render() {
+        return (
+            <div className={contentStyles.content}>
+                <Content {...this.props} profile={this.props.profile}></Content>
+            </div>
+        );
+    };
+};
+
+let MapStateToProps = (state) => ({profile: state.profilePage.profile});
+
+let WithUrlDataContainerComponent = withRouter(ContentContainer);
+
+export default connect(MapStateToProps, {setUserProfile})(withRouter(ContentContainer));
